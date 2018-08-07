061daa80a9a7325f5ec4ebb23e316b2368f939bf
Joey Cho <cjy12182000@naver.com>
2018-08-07 07:04:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,4 +28,23 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, bleed) VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql,self.name, self.bleed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]      
+    end
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, bleed = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.bleed, self.id)
+  end
+
 end
