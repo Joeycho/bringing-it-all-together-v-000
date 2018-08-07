a02961885952719e729225aedee6f644442db949
Joey Cho <cjy12182000@naver.com>
2018-08-07 07:36:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ class Dog
     dog_o = self.new(id: dog[0],name: dog[1],breed: dog[2])
   end
 
-  def self.find_or_create_by(name:, breed: )
+  def self.find_or_create_by(name, breed)
     sql = <<-SQL
       SELECT * FROM dogs WHERE name = ?, breed = ?
     SQL
